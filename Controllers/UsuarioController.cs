119f828adc9c2006f0fcc5135ef7a93414e1f6bc
gabriel-artemio <gabs.artemioferreira@gmail.com.br>
2023-01-05 23:18:23 -0300

    primeiro commit

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using ASP.NET_webAPi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_webAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Usuario>> GetALl()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Models/Tarefa.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP.NET_webAPi.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string? Nm_tarefa { get; set; }
+        public string? Descricao { get; set; }
+        public int Status { get; set; }
+    }
+}
