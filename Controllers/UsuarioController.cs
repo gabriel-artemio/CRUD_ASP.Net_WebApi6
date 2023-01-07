3dbbbe7f7aaf308dfbcc6a0dc9a462ef78d53bc4
gabriel-artemio <gabs.artemioferreira@gmail.com.br>
2023-01-07 01:10:36 -0300

    ajustes

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
 ﻿using ASP.NET_webAPi.Models;
+using ASP.NET_webAPi.Repositorios.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,10 +9,42 @@ namespace ASP.NET_webAPi.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<Usuario>> GetALl()
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio)
         {
-            return Ok();
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Usuario>>>GetAll()
+        {
+            List<Usuario>usuarios = await _usuarioRepositorio.GetUsuarios();
+            return Ok(usuarios);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> GetById(int id)
+        {
+            Usuario usuario = await _usuarioRepositorio.GetById(id);
+            return Ok(usuario);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> Insert([FromBody]Usuario user)
+        {
+            Usuario usuario = await _usuarioRepositorio.Insert(user);
+            return Ok(usuario);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Usuario>> Update([FromBody] Usuario user, int id)
+        {
+            user.Id= id;
+            Usuario usuario = await _usuarioRepositorio.Update(user, id);
+            return Ok(usuario);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Usuario>> Delete(int id)
+        {
+            bool deletado = await _usuarioRepositorio.Delete(id);
+            return Ok(deletado);
         }
     }
 }
