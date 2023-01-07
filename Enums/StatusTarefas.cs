baecd69e24b39be3d4e3ccc7f50c9e3c0fb3bd3b
gabriel-artemio <gabs.artemioferreira@gmail.com.br>
2023-01-06 22:00:12 -0300

    ajustes

---
+++ Enums/StatusTarefas.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace ASP.NET_webAPi.Enums
+{
+    public enum StatusTarefas
+    {
+        [Description("A fazer")]
+        Afazer = 1,
+        [Description("Em andamento")]
+        EmAndamento = 2,
+        [Description("Concluído")]
+        Concluido = 3
+    }
+}
--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -1,10 +1,12 @@
-﻿namespace ASP.NET_webAPi.Models
+﻿using ASP.NET_webAPi.Enums;
+
+namespace ASP.NET_webAPi.Models
 {
     public class Tarefa
     {
         public int Id { get; set; }
         public string? Nm_tarefa { get; set; }
         public string? Descricao { get; set; }
-        public int Status { get; set; }
+        public StatusTarefas Status { get; set; }
     }
 }
